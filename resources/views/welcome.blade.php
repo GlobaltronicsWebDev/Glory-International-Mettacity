1e6a73b7f5de05498c84897cb0436702e20d972e
Web Developer / MIS <97711236+GlobaltronicsWebDev@users.noreply.github.com>
2026-01-06 11:38:01 +0800

    TITLEADDWEBSITE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'My Laravel 12 App')</title>
+    <title>@yield('title', 'GLORY INTERNATIONAL METACITY')</title>
     <!-- Include CSS using the asset helper for proper pathing -->
     <link rel="stylesheet" href="{{ asset('cssfolder/main.css') }}">
 </head>
