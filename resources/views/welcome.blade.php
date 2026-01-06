785d908b3143d798c6a9bf24bd19dc3360cad4d3
Web Developer / MIS <97711236+GlobaltronicsWebDev@users.noreply.github.com>
2026-01-06 17:02:04 +0800

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'GLORY INTERNATIONAL METACITY')</title>
     <!-- Include CSS using the asset helper for proper pathing -->
-    <link rel="stylesheet" href="{{ asset('cssfolder\main.css') }}">
+   <link rel="stylesheet" href="{{ asset('cssfolder/main.css') }}">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
 </head>
 <body>
