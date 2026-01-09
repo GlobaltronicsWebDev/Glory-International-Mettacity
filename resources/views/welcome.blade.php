caca5f4ba37592d9d867d3c03d0934cb25ccb70b
Web Developer / MIS <97711236+GlobaltronicsWebDev@users.noreply.github.com>
2026-01-09 09:53:17 +0800

    asd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GLORY INTERNATIONAL METTACITY</title>
-     <link rel="stylesheet" href="{{ asset('public/favicon.ico') }}">
+     <link rel="icon" href="{{ asset('public/favicon.ico') }}">
 </head>
 <body>
     
